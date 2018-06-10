3ffe56a5f6c79a8422de550dc31f39ed4c5d60a9
Lucas Simas <simas.lucas@hotmail.com>
2018-06-10 16:38:32 -0300

    Description

--- Mobile/EasyStreets/app/src/main/java/unicap/es/easystreets/model/StreetFurniture.java
+++ Mobile/EasyStreets/app/src/main/java/unicap/es/easystreets/model/StreetFurniture.java
@@ -6,69 +6,73 @@ import unicap.es.easystreets.R;
 
 public enum StreetFurniture {
     @SerializedName("0")
-    BusStop(R.drawable.bus_stop),                       //pontos de ônibus
+    BusStop(R.drawable.bus_stop, "Bus Stop"),                       //pontos de ônibus
     @SerializedName("1")
-    TaxiPoints(R.drawable.bus_stop),                     //pontos de táxi
+    TaxiPoints(R.drawable.bus_stop, ""),                     //pontos de táxi
     @SerializedName("2")
-    MailBoxes(R.drawable.bus_stop),                      //caixas de coleta de correio
+    MailBoxes(R.drawable.bus_stop, ""),                      //caixas de coleta de correio
     @SerializedName("3")
-    Hydrants(R.drawable.bus_stop),                       //hidrantes
+    Hydrants(R.drawable.bus_stop, ""),                       //hidrantes
     @SerializedName("4")
-    TelephoneNetworkCabinets(R.drawable.bus_stop),       //armários da rede telefônica
+    TelephoneNetworkCabinets(R.drawable.bus_stop, ""),       //armários da rede telefônica
     @SerializedName("5")
-    ElectricalNetworkCabinets(R.drawable.bus_stop),      //armários da rede elétrica
+    ElectricalNetworkCabinets(R.drawable.bus_stop, ""),      //armários da rede elétrica
     @SerializedName("6")
-    Seats(R.drawable.bus_stop),                          //bancos com ou sem costas
+    Seats(R.drawable.bus_stop, ""),                          //bancos com ou sem costas
     @SerializedName("7")
-    Vase(R.drawable.bus_stop),                           //vasos
+    Vase(R.drawable.bus_stop, ""),                           //vasos
     @SerializedName("8")
-    Trash(R.drawable.bus_stop),                          //lixeiras ou papeleiras
+    Trash(R.drawable.bus_stop, ""),                          //lixeiras ou papeleiras
     @SerializedName("9")
-    LightingPoles(R.drawable.bus_stop),                  //postes de iluminação
+    LightingPoles(R.drawable.bus_stop, ""),                  //postes de iluminação
     @SerializedName("10")
-    PowerPoles(R.drawable.bus_stop),                     //postes da rede elétrica
+    PowerPoles(R.drawable.bus_stop, ""),                     //postes da rede elétrica
     @SerializedName("11")
-    SignPosts(R.drawable.bus_stop),                      //postes de sinalização
+    SignPosts(R.drawable.bus_stop, ""),                      //postes de sinalização
     @SerializedName("12")
-    BikeSpot(R.drawable.bus_stop),                       //apoios ou parqueamento de bicicletas
+    BikeSpot(R.drawable.bus_stop, ""),                       //apoios ou parqueamento de bicicletas
     @SerializedName("13")
-    Beacon(R.drawable.bus_stop),                         //divisores, guias e balizadores (fradinhos, pilones, etc)
+    Beacon(R.drawable.bus_stop, ""),                         //divisores, guias e balizadores (fradinhos, pilones, etc)
     @SerializedName("14")
-    WaterSource(R.drawable.bus_stop),                    //fontes ou bebedouros
+    WaterSource(R.drawable.bus_stop, ""),                    //fontes ou bebedouros
     @SerializedName("15")
-    Newsstands(R.drawable.bus_stop),                     //bancas de jornal
+    Newsstands(R.drawable.bus_stop, ""),                     //bancas de jornal
     @SerializedName("16")
-    FlowerStands(R.drawable.bus_stop),                   //bancas de flores ou floreiras
+    FlowerStands(R.drawable.bus_stop, ""),                   //bancas de flores ou floreiras
     @SerializedName("17")
-    Clocks(R.drawable.bus_stop),                         //relógios
+    Clocks(R.drawable.bus_stop, ""),                         //relógios
     @SerializedName("18")
-    TablesWithBenches(R.drawable.bus_stop),              //mesas com bancos
+    TablesWithBenches(R.drawable.bus_stop, ""),              //mesas com bancos
     @SerializedName("19")
-    Handrails(R.drawable.bus_stop),                      //guardas e corrimãos
+    Handrails(R.drawable.bus_stop, ""),                      //guardas e corrimãos
     @SerializedName("20")
-    Grills(R.drawable.bus_stop),                         //grelhas para caldeiras de árvores
+    Grills(R.drawable.bus_stop, ""),                         //grelhas para caldeiras de árvores
     @SerializedName("21")
-    ShadingStructures(R.drawable.bus_stop),              //estruturas de sombreamento
+    ShadingStructures(R.drawable.bus_stop, ""),              //estruturas de sombreamento
     @SerializedName("22")
-    Dispenser(R.drawable.bus_stop),                      //dispensador de sacos para dejetos caninos
+    Dispenser(R.drawable.dispensa_degetos, "Dispenser"),     //dispensador de sacos para dejetos caninos
     @SerializedName("23")
-    InformationMedia(R.drawable.bus_stop),               //suportes informativos e expositores
+    InformationMedia(R.drawable.bus_stop, ""),               //suportes informativos e expositores
     @SerializedName("24")
-    GymStructures(R.drawable.bus_stop);                  //estruturas de ginástica para seniores
+    GymStructures(R.drawable.bus_stop, "");                  //estruturas de ginástica para seniores
 
     private int resource;
+    private String description;
 
-    StreetFurniture(int resource){
+    StreetFurniture(int resource, String description){
         this.resource = resource;
+        this.description = description;
     }
 
     public int getResource(){
         return this.resource;
     }
 
+    public String getDescription(){
+        return this.description;
+    }
+    
     public int toValue() {
         return ordinal();
     }
-
-
 }
