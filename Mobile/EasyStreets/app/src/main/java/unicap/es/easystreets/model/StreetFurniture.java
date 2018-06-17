0a2ea4cf738a54d747fe989ecae9dae60cee9e4a
athoshenrique <athoshenrique2003@gmail.com>
2018-06-16 21:36:41 -0300

    Tradução markers
    Trad

--- Mobile/EasyStreets/app/src/main/java/unicap/es/easystreets/model/StreetFurniture.java
+++ Mobile/EasyStreets/app/src/main/java/unicap/es/easystreets/model/StreetFurniture.java
@@ -6,55 +6,55 @@ import unicap.es.easystreets.R;
 
 public enum StreetFurniture {
     @SerializedName("0")
-    BusStop(R.drawable.bus_stop, "Bus Stop"),                       //pontos de ônibus
+    BusStop(R.drawable.bus_stop, "Ponto de ônibus"),                       //pontos de ônibus
     @SerializedName("1")
-    TaxiPoints(R.drawable.taxi_stand, "Taxi Point"),                     //pontos de táxi
+    TaxiPoints(R.drawable.taxi_stand, "Ponto de táxi"),                     //pontos de táxi
     @SerializedName("2")
-    MailBoxes(R.drawable.mail_box, "Mail Box"),                      //caixas de coleta de correio
+    MailBoxes(R.drawable.mail_box, "Caixa de correio"),                      //caixas de coleta de correio
     @SerializedName("3")
-    Hydrants(R.drawable.hydrant, "Hydrant"),                       //hidrantes
+    Hydrants(R.drawable.hydrant, "Hidrante"),                       //hidrantes
     @SerializedName("4")
-    TelephoneNetworkCabinets(R.drawable.telephone, "Telephone Network Cabinet"),       //armários da rede telefônica
+    TelephoneNetworkCabinets(R.drawable.telephone, "Caixa de rede telefônica"),       //armários da rede telefônica
     @SerializedName("5")
-    ElectricalNetworkCabinets(R.drawable.eletrical_netw_cabinet, "Eletrical Network Cabinets"),      //armários da rede elétrica
+    ElectricalNetworkCabinets(R.drawable.eletrical_netw_cabinet, "Caixa de rede elétrica"),      //armários da rede elétrica
     @SerializedName("6")
-    Seats(R.drawable.seat, "Seat"),                          //bancos com ou sem costas
+    Seats(R.drawable.seat, "Bancos de sentar"),                          //bancos com ou sem costas
     @SerializedName("7")
-    Vase(R.drawable.vase, "Vase"),                           //vasos
+    Vase(R.drawable.vase, "Vaso"),                           //vasos
     @SerializedName("8")
-    Trash(R.drawable.trash, "Trash"),                          //lixeiras ou papeleiras
+    Trash(R.drawable.trash, "Cesto de lixo"),                          //lixeiras ou papeleiras
     @SerializedName("9")
-    LightingPoles(R.drawable.ligh_poles, "Lighting Poles"),                  //postes de iluminação
+    LightingPoles(R.drawable.ligh_poles, "Poste de iluminação"),                  //postes de iluminação
     @SerializedName("10")
-    PowerPoles(R.drawable.power_poles, "Power Poles"),                     //postes da rede elétrica
+    PowerPoles(R.drawable.power_poles, "Poste de rede elétrica"),                     //postes da rede elétrica
     @SerializedName("11")
-    SignPosts(R.drawable.sign_posts, "Sign Post"),                      //postes de sinalização
+    SignPosts(R.drawable.sign_posts, "Poste de sinalização"),                      //postes de sinalização
     @SerializedName("12")
-    BikeSpot(R.drawable.bike_spot, "Bike Spot"),                       //apoios ou parqueamento de bicicletas
+    BikeSpot(R.drawable.bike_spot, "Parqueamento de bike"),                       //apoios ou parqueamento de bicicletas
     @SerializedName("13")
-    Beacon(R.drawable.handrails, "Beacon"),                         //divisores, guias e balizadores (fradinhos, pilones, etc)
+    Beacon(R.drawable.handrails, "Balisadores"),                         //divisores, guias e balizadores (fradinhos, pilones, etc)
     @SerializedName("14")
-    WaterSource(R.drawable.water_source, "Water Source"),                    //fontes ou bebedouros
+    WaterSource(R.drawable.water_source, "Bebedouros"),                    //fontes ou bebedouros
     @SerializedName("15")
-    Newsstands(R.drawable.news_stand, "News Stand"),                     //bancas de jornal
+    Newsstands(R.drawable.news_stand, "Banca de jornal"),                     //bancas de jornal
     @SerializedName("16")
-    FlowerStands(R.drawable.flower_stand, "Flower Stand"),                   //bancas de flores ou floreiras
+    FlowerStands(R.drawable.flower_stand, "Banca de flores"),                   //bancas de flores ou floreiras
     @SerializedName("17")
-    Clocks(R.drawable.clock, "Clock"),                         //relógios
+    Clocks(R.drawable.clock, "Relógio"),                         //relógios
     @SerializedName("18")
-    TablesWithBenches(R.drawable.table_with_benches, "Table with benches"),              //mesas com bancos
+    TablesWithBenches(R.drawable.table_with_benches, "Mesa com cadeiras"),              //mesas com bancos
     @SerializedName("19")
-    Handrails(R.drawable.handrails, "Handrails"),                      //guardas e corrimãos
+    Handrails(R.drawable.handrails, "Corrimãos"),                      //guardas e corrimãos
     @SerializedName("20")
-    Grills(R.drawable.grill, "Grill"),                         //grelhas para caldeiras de árvores
+    Grills(R.drawable.grill, "Grelhas"),                         //grelhas para caldeiras de árvores
     @SerializedName("21")
-    ShadingStructures(R.drawable.shading_struct, "Shading Structures"),              //estruturas de sombreamento
+    ShadingStructures(R.drawable.shading_struct, "Estrutura de sombreamento"),              //estruturas de sombreamento
     @SerializedName("22")
-    Dispenser(R.drawable.dispensa_degetos, "Dispenser"),     //dispensador de sacos para dejetos caninos
+    Dispenser(R.drawable.dispensa_degetos, "Lixo para degetos de animais"),     //dispensador de sacos para dejetos caninos
     @SerializedName("23")
-    InformationMedia(R.drawable.information, "Information Media"),               //suportes informativos e expositores
+    InformationMedia(R.drawable.information, "Suportes informativos"),               //suportes informativos e expositores
     @SerializedName("24")
-    GymStructures(R.drawable.gym, "Gym Structures");                  //estruturas de ginástica para seniores
+    GymStructures(R.drawable.gym, "Estrutura de ginástica");                  //estruturas de ginástica para seniores
 
     private int resource;
     private String description;
